fd8c2d05b6fe62fe3651abf50af652e2dd36bdad
AlionaMu <alionazimaby@gmail.com>
2023-08-07 22:48:06 +0300

    init: start task

---
+++ getter-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+GETTER APP
+      </header>
+    </div>
+  );
+}
+
+export default App;
